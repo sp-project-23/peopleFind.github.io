7a0956871fa423627c2f917ef17486fc2527b44d
Sourav <133347351+sp-project-23@users.noreply.github.com>
2023-06-20 20:23:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+
+import data from './data.json' assert { type: 'json' };
+
+
+var data_size = data.length;
+var pages_required = data.length/3;
+
+function createCard(data, serial)
+{
+    let li = document.createElement("li");
+    let div = document.createElement("div");
+    let span = document.createElement("span");
+
+    div.appendChild(span);
+
+    let div2 = div.cloneNode(true);
+    let div3 = document.createElement("div");
+
+    li.appendChild(div3);
+    li.appendChild(div);
+    li.appendChild(div2);
+
+    div.children[0].innerText = data.name;
+    div2.children[0].innerText = data.location;
+    div3.innerText = serial;
+    
+    div3.classList.add("serial");
+    div.classList.add("name");
+    div2.classList.add("loc");
+
+    return li;
+}
+
+let ul = document.getElementById("card");
+
+function paginate(array, page_size, page_number) {
+    // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
+    return array.slice((page_number - 1) * page_size, page_number * page_size);
+}
+
+
+var page = 0;
+nextCard(ul);
+
+function nextCard(parent){
+
+    if(page<=pages_required){
+        
+        let items = paginate(data, 3, ++page);
+        console.log(items);
+        parent.innerHTML = "";
+        items.forEach((element, index) => {
+            parent.appendChild(createCard(element, (page*3)-(3-index)+1));
+        });
+
+        document.getElementById("note").innerText = `CURRENTLT ${items.length} PEOPLE SHOWING`;
+
+    }
+    else
+        document.getElementById("note").innerText = `NO MORE PEOPLE!`;
+}
+
+document.getElementById("btn").addEventListener("click", ()=>nextCard(ul));
