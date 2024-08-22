ecee88661aca65c1486c5447aea34f4f5b7a81a7
Sourav <sp.manick19@gmail.com>
2024-08-22 20:49:29 +0530

    do

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import data from 'https://sp-project-23.github.io/peopleFind.github.io/data.json' assert { type: 'json' };
-console.log(data);
+// console.log(data);
 
 var data_size = data.length;
 var pages_required = data.length/3;
